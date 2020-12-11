c74d5ec95df81ce86f8fee41b3aa23275898ee5a
Jehiel Martinez <jehielmartinez@gmail.com>
2020-12-11 17:17:28 -0600

    code challenge 11-12-20

--- christmasTree.js
+++ christmasTree.js
@@ -8,6 +8,24 @@
 // Conditions: To draw the Christmas tree you should use only special chapters
 // Will simulate changes light as a real tree
 
+// USE ONLY IN VS CODE TERMINAL!!!!!
+// USE ONLY UNDER YOUR OWN RISK
+// TODO: Support other terminals
+
 const drawATree = (height) => {
-  return true
+  let padding = 0
+
+  for (let i = height; i > 0; i--) {
+    const char = i === height ? '⭐️' : i % 3 === 0 ? '💡' : '🎄'
+    const line = ' '.repeat(i) + char + char.repeat(height - i + ' ')
+    padding = ~~(height / 2)
+    console.log(line)
+  }
+
+  for (let i = 3; i > 0; i--) {
+    console.log(' '.repeat(padding) + '|'.repeat(padding * 2))
+  }
+  console.log(' '.repeat(padding) + '** Merry Christmas **')
 }
+
+drawATree(50)
