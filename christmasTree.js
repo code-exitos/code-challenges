eed204e89c11061d537bfdd4b899281e8d1c80be
Jehiel Martinez <jehielmartinez@gmail.com>
2020-12-11 13:27:20 -0600

    first challenge description

--- christmasTree.js
+++ christmasTree.js
@@ -1,1 +1,13 @@
-// Draw a Christmas tree in the Console.
+// Draw a Christmas tree in the Console using a function that will receive a height value
+//     x
+//    xxx
+//   xxxxx
+//  xxxxxxx
+// xxxxxxxxx
+
+// Conditions: To draw the Christmas tree you should use only special chapters
+// Will simulate changes light as a real tree
+
+const drawATree = (height) => {
+  return true
+}
