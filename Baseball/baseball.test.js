f9fd88166501d4e653435eecfa382360c0e4f7e3
jehielmartinez <jehielmartinez@gmail.com>
2021-01-05 16:12:15 -0600

    The program can switch sides
    Co-authored-by: Daniel Perez <iamdperez@users.noreply.github.com>
    Co-authored-by: Erick Gonzales <StriderHND@users.noreply.github.com>

--- Baseball/baseball.test.js
+++ Baseball/baseball.test.js
@@ -1,10 +1,12 @@
-const ScoreCard = require('./baseball');
+const {ScoreCard, Team} = require('./baseball');
 
 describe('ScoreCard', () => {
     let scoreCard;
+    let team;
 
     beforeEach(() => {
       scoreCard = new ScoreCard();
+      team = new Team()
     });
 
     it('should construct a ScoreCard', () => {
@@ -19,4 +21,39 @@ describe('ScoreCard', () => {
         scoreCard.addEntry(4);
         expect(scoreCard.getScore()).toBe('Home: 0 Away: 1');
     });
+
+    it('Should increment runs for homerun', () => {
+        scoreCard.addEntry(4);
+        scoreCard.addEntry(4);
+        expect(scoreCard.getScore()).toBe('Home: 0 Away: 2');
+    });
+
+    it('Should switch side after three outs', () => {
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(4);
+        expect(scoreCard.getScore()).toBe('Home: 1 Away: 0');
+    });
+    it('Should switch side twice', () => {
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(4);
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(0);
+        scoreCard.addEntry(4);
+        expect(scoreCard.getScore()).toBe('Home: 1 Away: 1');
+    });
+
+    it('should construct a Team', () => {
+        expect(team).toBeDefined();
+    });
+
+    it('team should display name and score', () => {
+        const aTeam = new Team("Foo");
+        expect(aTeam.toString()).toBe("Foo: 0");
+    });
+
 })
