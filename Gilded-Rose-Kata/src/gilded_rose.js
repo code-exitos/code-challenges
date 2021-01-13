999248ec17e7cd7924379a9e95fe54ea37048d5a
Jehiel Martinez <jehielmartinez@gmail.com>
2021-01-13 17:00:14 -0600

    end of session 13 January

--- Gilded-Rose-Kata/src/gilded_rose.js
+++ Gilded-Rose-Kata/src/gilded_rose.js
@@ -6,61 +6,87 @@ class Item {
   }
 }
 
+class AgiedBrie extends Item {
+  constructor(sellIn, quality){
+    super('Aged Brie', sellIn, quality)
+  }
+  updateQuality(){
+    if(this.quality < 50){
+      this.quality += 1;
+    }
+    if(this.sellIn < 0){
+      this.quality += 1;
+    }
+  }
+}
+
 class Shop {
   constructor(items = []) {
     this.items = items;
     this.BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    this.AGIED_BRIE = "Aged Brie";
+    this.AGIED_BRIE = "Agied Brie";
     this.SULFURAS = "Sulfuras, Hand of Ragnaros";
   }
+
+  changeQualityOf(item, value = 1) {
+    if (item.quality < 50) item.quality += value;
+  }
+
+  changeSellInOf(item) {
+    if (item.name !== this.SULFURAS) {
+      item.sellIn -= 1;
+    }
+  }
+
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      const name = this.items[i].name;
+    this.items.forEach((item) => {
+      const name = item.name;
+
+      if(name == this.AGIED_BRIE){
+        item.updateQuality();
+      }
+      
       if (
-        name != this.AGIED_BRIE &&
-        name != this.BACKSTAGE_PASSES
+        ![this.AGIED_BRIE, this.BACKSTAGE_PASSES, this.SULFURAS].includes(name) &&
+        item.quality > 0
       ) {
-        if (this.items[i].quality > 0 && name != this.SULFURAS) {
-            this.items[i].quality -= 1;
-        }
+        this.changeQualityOf(item, -1);
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality += 1;
-          if (name == this.BACKSTAGE_PASSES) {
-            if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-              this.items[i].quality += 1;
-            }
-            if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
-              this.items[i].quality += 1;
-            }
+        if(name !== this.AGIED_BRIE){
+          this.changeQualityOf(item);
+        }
+        if (name === this.BACKSTAGE_PASSES) {
+          if (item.sellIn < 6) {
+            this.changeQualityOf(item, 2);
+          } else if (item.sellIn < 11) {
+            this.changeQualityOf(item);
           }
         }
       }
-      if (name != this.SULFURAS) {
-        this.items[i].sellIn -= 1;
-      }
-      if (this.items[i].sellIn < 0) {
+      
+      this.changeSellInOf(item);
+
+      if (item.sellIn < 0) {
         if (name != this.AGIED_BRIE) {
           if (name != this.BACKSTAGE_PASSES) {
-            if (this.items[i].quality > 0 && name != this.SULFURAS) {
-                this.items[i].quality -= 1;
+            if (item.quality > 0 && name != this.SULFURAS) {
+              this.changeQualityOf(item, -1);
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            item.quality = 0;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality += 1;
-          }
+          // this.changeQualityOf(item);
         }
       }
-    }
+      
+    });
 
     return this.items;
   }
 }
-
 module.exports = {
   Item,
   Shop,
+  AgiedBrie
 };
