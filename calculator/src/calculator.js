4e019311503a2f865cee4f57fb5b7b00a44cdd91
Erick Gonzales <erickgon45@gmail.com>
2021-01-18 15:48:42 -0600

    added charToNumber function

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -2,4 +2,15 @@ const convertToDecimal = () => {
  return 1
 }
 
+const charToNumber = (char) => {
+    switch (char) {
+        case 'I':
+            return 1
+            break;
+        default:
+            return -1
+            break;
+    }
+}
+
 module.exports = convertToDecimal
