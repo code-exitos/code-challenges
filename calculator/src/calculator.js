53be59d523a943d5ee5a603092a4f61f961eb05a
jehielmartinez <jehielmartinez@gmail.com>
2021-01-19 16:11:39 -0600

    latest 19/02/2021 mob programming

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -1,19 +1,29 @@
-const convertToDecimal = () => {
- return 1
-}
 
 const charToNumber = (char) => {
     switch (char) {
         case 'I':
             return 1
-            break;
-        case 'II':
-            return 2
-            break;
+        case 'V':
+            return 5
+        case 'X':
+            return 10
+        case 'L':
+            return 50
         default:
             return -1
-            break;
     }
+}
+const convertToDecimal = (str) => {
+    let number=0;
+    for(let i = 0; i < str.length; i++){
+        const nextChar = str.charAt(i+1)
+        if(nextChar === 'X' || nextChar === 'V'){
+            number-=2
+        }
+        number+=charToNumber(str.charAt(i))
+    }
+
+  return number;
 }
 
 module.exports = {convertToDecimal, charToNumber}
--- calculator/src/calculator.test.js
+++ calculator/src/calculator.test.js
@@ -1,19 +1,51 @@
-const {convertToDecimal, charToNumber}  = require('./calculator');
-describe('Roman Calculator', () => {
-  it('should return one', () => {
-    expect(convertToDecimal()).toBe(1)
-  })
 
+const {convertToDecimal} = require('./calculator');
+describe('Roman Calculator', () => {
   it('should return one if the value is I', () => {
-      expect(charToNumber('I')).toBe(1)
+      expect(convertToDecimal('I')).toBe(1)
   })
 
   it('should return two if the value is II', () => {
-    expect(charToNumber('II')).toBe(2)
-})
-it('should return two if the value is II', () => {
-  expect(charToNumber('II')).toBe(2)
-})
+    expect(convertToDecimal('II')).toBe(2)
+  })
 
+  it('should return three if the value is III', () => {
+    expect(convertToDecimal('III')).toBe(3)
+  })
 
+  it('should returnn four if the value is IV', () => {
+    expect(convertToDecimal('IV')).toBe(4)
+  })
+
+  it('should returnn five if the value is V', () => {
+    expect(convertToDecimal('V')).toBe(5)
+  })
+
+  it('should returnn six if the value is VI', () => {
+    expect(convertToDecimal('VI')).toBe(6)
+  })
+
+  it('should returnn seven if the value is VII', () => {
+    expect(convertToDecimal('VII')).toBe(7)
+  })
+
+  it('should returnn eight if the value is VIII', () => {
+    expect(convertToDecimal('VIII')).toBe(8)
+  })
+
+  it('should returnn nineten if the value is IX', () => {
+    expect(convertToDecimal('IX')).toBe(9)
+  })
+
+  it('should returnn seven if the value is X', () => {
+    expect(convertToDecimal('X')).toBe(10)
+  })
+
+  it('should return fifty if the value is L', () => {
+    expect(convertToDecimal('L')).toBe(50)
+  })
+
+  it('should return forty-five if the value is XLV', () => {
+    expect(convertToDecimal('XLV')).toBe(45)
+  })
 })
